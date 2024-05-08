807b310cc73a4df12f10a8e7dbac85a0d3b56722
a.skityaeva <a.skityaeva@itcodegroup.ru>
2024-05-08 18:22:13 +0500

    Удаление файлов проекта

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
-
-def renderDate(request):
-    now = datetime.now()
-    html = "<html><body>Текущая дата: %s</body></html>" % now
-    return HttpResponse(html)
